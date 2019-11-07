9b08c4b06e7b9b8c3c1b21830e3176c54fda5059
andri <andri@DESKTOP-UDAFHCA>
2019-11-07 22:09:19 +0700

    perubahan

--- TestLinked/src/Test.java
+++ TestLinked/src/Test.java
@@ -1,14 +1,21 @@
 
 public class Test {
 	public static void main(String[] args) {
+		
+		
 		Node a = new Node();
 		Node b = new Node();
+		Node c = new Node();
 		
 		a.setValue(8);
 		b.setValue(10);
+		c.setValue(12);
 		
 		a.setNext(b);
-		System.out.println(a.getValue() +" "+a.getNext());
+		b.setNext(c);
+		
+		
+		System.out.println(a.getValue() +" "+a.getNext().getValue() +" "+ a.getNext().getNext().getValue());
 		
 		
 	}
